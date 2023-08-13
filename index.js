d9683fb37d701a16e96843413be1bd82f4d50e0e
triggered-exe <harishankersharma648@gmail.com>
2023-08-13 14:54:55 +0530

    added checkauthentication middleware to profile page and signin and signup pages will check if already authenticated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require("./config/mongoose.js");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy.js");
-const MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo");
 
 app.use(cookieParser());
 // for json
@@ -25,16 +25,32 @@ app.use(express.static("./assets"));
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
+//setting the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+
+//MongoStore is used to store the session cookie in the db
 app.use(session({
     name: "Konnect",
     //change me
     secret: "change",
     saveUninitialized: false,
-    resave: false, 
+    resave: false,
     cookie: {
-        maxAge: 1000*60*100 
-    }
-}))
+        maxAge: 1000 * 60 * 100
+    },
+    store: MongoStore.create(
+            {
+                mongoUrl: 'mongodb://127.0.0.1/Konnect_development',
+                autoRemove: "disabled"
+            },
+            function(error) {
+                console.log(error || `connect-mongodb setup ok`);
+            }
+    )
+
+    }))
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -44,9 +60,8 @@ app.use(passport.setAuthenticatedUser);
 //use express  router
 app.use("/", require("./routes/index.js"));
 
-//setting the view engine
-app.set("view engine", "ejs");
-app.set("views", "./views");
+
+
 
 
 
