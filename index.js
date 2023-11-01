8cef7deec92eb1e3d77630eb8e43f9aaf9b7b485
triggered-exe <harishankersharma648@gmail.com>
2023-11-01 21:35:45 +0530

    render deploy

--- index.js
+++ index.js
@@ -117,9 +117,9 @@ const expressServer = app.listen(PORT, (err) => {
 //  setup the char server using socket.io
 
 const io = new Server(expressServer, {
-    // cors: {
-    //     origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
-    // }
+    cors: {
+        origin:["https://konnect-odr1.onrender.com:10000","https://konnect-odr1.onrender.com","http://localhost:5500", "http://127.0.0.1:5500"]
+    }
 })
 
 chatServerListener(io);
